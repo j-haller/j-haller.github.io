e0bc49324b152dfa6f614ffb70afcfff798d2acc
Jakob Haller <haller.jakob@outlook.de>
2024-12-20 13:27:51 +0100

    Brainstorming

--- test/ray-casting.test.js
+++ test/ray-casting.test.js
@@ -10,31 +10,33 @@ describe('ray-casting', () => {
 
     before(() => {
         polygon = new Boundary([
-            { x: 0, y: 0 },
-            { x: 5, y: 0 },
+            { x: 1, y: 1 },
+            { x: 5, y: 1 },
             { x: 5, y: 5 },
-            { x: 2.5, y: 2.5 },
-            { x: 0, y: 5 },
+            { x: 4, y: 4 },
+            { x: 2, y: 4 },
+            { x: 2, y: 5 },
+            { x: 1, y: 5 },
         ]);
         vertices = polygon.vertices;
     });
 
     it('should return false if point is outside the polygon', () => {
-        const point = { x: 2.5, y: 10 };
+        const point = { x: 3, y: 0 };
         const result = isInside(vertices, point);
         strictEqual(result, false);
+    });
+
+    it('should return true if point is inside the polygon', () => {
+        const point = { x: 3, y: 3 };
+        const result = isInside(vertices, point);
+        strictEqual(result, true);
     });
 
     it('should return false if point is outside the polygon but the ray crosses a node', () => {
         const point = { x: 2.5, y: 5 };
         const result = isInside(vertices, point);
         strictEqual(result, false);
-    });
-
-    it('should return true if point is inside the polygon', () => {
-        const point = { x: 2.5, y: 1 };
-        const result = isInside(vertices, point);
-        strictEqual(result, true);
     });
 
     it('should return false if point is exactly on a horizontal edge', () => {
