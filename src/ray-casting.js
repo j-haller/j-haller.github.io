e3e20956bbe8428610a18b50570c72541a54ae38
Jakob Haller <haller.jakob@outlook.de>
2025-01-07 15:47:54 +0100

    Got all ray-casting tests running

--- src/ray-casting.js
+++ src/ray-casting.js
@@ -3,7 +3,7 @@
 // Ray casting alogrithm to determine whether a point is inside a polygon or
 // outside of if. A horizontal ray is casted from the point in question to
 // infinity. If the point should lie on a edge or a node, then the result is
-// going to be false.
+// going to be true.
 
 // polygon: array of points
 function getEdges(polygon) {
@@ -19,44 +19,113 @@ function getEdges(polygon) {
     return edges;
 }
 
-function isIntersecting(edge, point) {
+function isOdd(value) {
+    return value % 2 ? true : false;
+}
+
+function isOnNode(point, edge) {
+    return point.x === edge.A.x && point.y == edge.A.y;
+}
+
+function isOnHorizontalEdge(point, edge) {
     const ax = edge.A.x;
     const ay = edge.A.y;
     const bx = edge.B.x;
     const by = edge.B.y;
 
-    // definitely no intersection with edge
-    if ((point.y <= ay && point.y <= by) || (point.y >= ay && point.y >= by)) {
-        return null;
+    // Edge is not horizontal
+    if (ay != by) {
+        return false;
     }
 
-    if (by === ay) {
-        if ((point.x > ax && point.x > bx) || (point.x < ax && point.x < bx)) {
-            return false;
-        } else {
-            return true;
-        }
+    // Point is not horizontally aligned with the edge
+    if (point.y != ay) {
+        return false;
     }
 
+    // Point is not on the edge
+    if ((point.x > ax && point.x > bx) || (point.x < ax && point.x < bx)) {
+        return false;
+    }
+
+    return true;
+}
+
+function isOnVerticalEdge(point, edge) {
+    const ax = edge.A.x;
+    const ay = edge.A.y;
+    const bx = edge.B.x;
+    const by = edge.B.y;
+
+    // Edge is not vertical
+    if (ax != bx) {
+        return false;
+    }
+
+    // Point is not vertically aligned with the edge
+    if (point.x != ax) {
+        return false;
+    }
+
+    // Point is not on the edge
+    if ((point.y > ay && point.y > by) || (point.y < ay && point.y < by)) {
+        return false;
+    }
+
+    return true;
+}
+
+function isHorizontalRayFromPointIntersectingEdge(point, edge) {
+    const ax = edge.A.x;
+    const ay = edge.A.y;
+    const bx = edge.B.x;
+    const by = edge.B.y;
+
+    // Point is above or below edge
+    if ((point.y > ay && point.y > by) || (point.y < ay && point.y < by)) {
+        return false;
+    }
+
+    // Point is on the right side of an edge
+    if (point.x > ax && point.x > bx) {
+        return false;
+    }
+
+    // Point is on the left side of the edge
+    if (point.x < ax && point.x < bx) {
+        return true;
+    }
+
+    // For any other edge with a finite and non-zero gradient
     const gradient = (bx - ax) / (by - ay);
     const intersectionX = gradient * (point.y - ay) + ax;
 
     return intersectionX > point.x;
 }
 
-function isOdd(value) {
-    return value % 2 ? true : false;
-}
-
 export function isInside(polygon, point) {
-    let intersections = 0;
     const edges = getEdges(polygon);
+    let intersections = 0;
 
     for (const edge of edges) {
-        if (isIntersecting(edge, point)) {
+        if (isOnNode(point, edge)) {
+            return true;
+        }
+
+        if (isOnHorizontalEdge(point, edge)) {
+            return true;
+        }
+
+        if (isOnVerticalEdge(point, edge)) {
+            return true;
+        }
+
+        if (isHorizontalRayFromPointIntersectingEdge(point, edge)) {
             intersections++;
         }
     }
 
+    // if number of intersections is odd, then the point is inside of the
+    // polygon
     return isOdd(intersections);
 }
--- test/ray-casting.test.js
+++ test/ray-casting.test.js
@@ -11,12 +11,12 @@ describe('ray-casting', () => {
     before(() => {
         polygon = new Boundary([
             { x: 1, y: 1 },
-            { x: 5, y: 1 },
-            { x: 5, y: 5 },
+            { x: 4, y: 1 },
             { x: 4, y: 4 },
+            { x: 3, y: 3 },
+            { x: 2, y: 3 },
             { x: 2, y: 4 },
-            { x: 2, y: 5 },
-            { x: 1, y: 5 },
+            { x: 1, y: 4 },
         ]);
         vertices = polygon.vertices;
     });
@@ -28,32 +28,32 @@ describe('ray-casting', () => {
     });
 
     it('should return true if point is inside the polygon', () => {
-        const point = { x: 3, y: 3 };
+        const point = { x: 3, y: 2 };
         const result = isInside(vertices, point);
         strictEqual(result, true);
     });
 
     it('should return false if point is outside the polygon but the ray crosses a node', () => {
-        const point = { x: 2.5, y: 5 };
+        const point = { x: 3, y: 4 };
         const result = isInside(vertices, point);
         strictEqual(result, false);
     });
 
-    it('should return false if point is exactly on a horizontal edge', () => {
-        const point = { x: 2.5, y: 0 };
+    it('should return true if point is exactly on a horizontal edge', () => {
+        const point = { x: 3, y: 1 };
         const result = isInside(vertices, point);
-        strictEqual(result, false);
+        strictEqual(result, true);
     });
 
-    it('should return false if point is exactly on a vertical edge', () => {
-        const point = { x: 5, y: 2.5 };
+    it('should return true if point is exactly on a vertical edge', () => {
+        const point = { x: 1, y: 2 };
         const result = isInside(vertices, point);
-        strictEqual(result, false);
+        strictEqual(result, true);
     });
 
-    it('should return false if point is exactly on a node', () => {
-        const point = { x: 5, y: 5 };
+    it('should return true if point is exactly on a node', () => {
+        const point = { x: 4, y: 4 };
         const result = isInside(vertices, point);
-        strictEqual(result, false);
+        strictEqual(result, true);
     });
 });
